4596a48b8de97264af9fd42242952d8fbb10bffc
E. Lynette Rayle <elr37@cornell.edu>
2015-06-04 14:34:07 -0400

    FIX compile errors

--- src/main/java/edu/cornell/library/lc_converter/mapreduce/MarcConversionMapper.java
+++ src/main/java/edu/cornell/library/lc_converter/mapreduce/MarcConversionMapper.java
@@ -5,22 +5,25 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.String;
 import java.util.List;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.HashSet;
 import java.io.InputStream;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.Event;
 
 
-public class MarcConversionMapper extends Mapper<Text, Text, Text, LongWritable> {
+public class MarcConversionMapper <K> extends Mapper<K, Text, Text, Text>{
 
   @Override
   protected void map(K unused, Text urlText, Context context) throws IOException, InterruptedException {
     String urlString = urlText.toString();
     InputStream is = getUrl( urlString  );
-    HashSet<String> marcRecords = marcxmlCollectionParser( is ).iterator();
+    HashSet<String> marcRecords = this.marcxmlCollectionParser( is ).iterator();
 
     for( String marcXml = marcRecords.next(); marcRecords.hasNext(); marcXml = marcRecords.next() ){
         context.write(marcXml, null);
@@ -35,7 +38,7 @@ public class MarcConversionMapper extends Mapper<Text, Text, Text, LongWritable>
     while (r.hasNext()) {
       if (r.next() == XMLEvent.START_ELEMENT)
         if (r.getLocalName().equals("record")) {
-          marcRecords.add(r);
+          marcRecords.add(r.getText());
         }
     }
     return marcRecords;
