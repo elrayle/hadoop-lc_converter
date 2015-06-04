b1140a4f2c8ea92d4c382a316653e628972e3acc
E. Lynette Rayle <elr37@cornell.edu>
2015-06-04 19:34:33 -0400

    FIX packages

--- src/main/java/edu/cornell/library/lc_converter/mapreduce/WholeFileInputFormat.java
+++ src/main/java/edu/cornell/library/lc_converter/mapreduce/WholeFileInputFormat.java
@@ -1,10 +1,10 @@
 package edu.cornell.library.lc_converter.mapreduce;
 
 import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.CombineFileRecordReader;
+import org.apache.hadoop.mapreduce.lib.input.CombineFileRecordReader;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.CombineFileSplit;
+import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
