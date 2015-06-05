8b535de651974a97eb1327ad54911e9cda10c5a8
E. Lynette Rayle <elr37@cornell.edu>
2015-06-05 12:47:16 -0400

    Increase min split size

--- src/main/java/edu/cornell/library/lc_converter/mapreduce/MarcConversionJob.java
+++ src/main/java/edu/cornell/library/lc_converter/mapreduce/MarcConversionJob.java
@@ -27,8 +27,9 @@ public class MarcConversionJob extends Configured implements Tool {
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-    job.setInputFormatClass(SequenceFileInputFormat.class);
-//    job.setInputFormatClass(TextInputFormat.class);
+//    job.setInputFormatClass(SequenceFileInputFormat.class);
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setMinSplitSize(50000);
 //    job.setInputFormatClass(TWPInputFormat.class);
 //    job.setInputFormatClass(WholeFileInputFormat.class);
 
