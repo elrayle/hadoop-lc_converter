3204c399feb5efcb952345b4736b1f9646748656
elrayle <elr37@cornell.edu>
2015-06-11 16:18:53 -0400

    adjust max split

--- src/main/java/edu/cornell/library/lc_converter/mapreduce/MarcConversionJob.java
+++ src/main/java/edu/cornell/library/lc_converter/mapreduce/MarcConversionJob.java
@@ -31,7 +31,7 @@ public class MarcConversionJob extends Configured implements Tool {
     job.setInputFormatClass(KeyValueTextInputFormat.class);
 //    job.setInputFormatClass(TWPInputFormat.class);
 //    job.setInputFormatClass(WholeFileInputFormat.class);
-    FileInputFormat.setMaxInputSplitSize(job,24);
+    FileInputFormat.setMaxInputSplitSize(job,20);
 
     job.setMapperClass(MarcConversionMapper.class);
     job.setNumReduceTasks(0);
